e8bf1ffe505200a46307948a079eb3ad83adaf68
Shaik Alima Zabi <shaikalimazabi@gmail.com>
2025-12-18 12:09:18 +0530

    Fix module import for production deployment

--- url_shortner/app.py
+++ url_shortner/app.py
@@ -1,7 +1,7 @@
 from flask import Flask, request, redirect, render_template
 import random
 import string
-from models import (
+from .models import (
     init_db, 
     insert_url, 
     get_url, 
